84caf7ae29b332376138366063f2e7b434302a36
dioptre <dioptre@gmail.com>
2020-04-13 20:17:03 -0700

    wip

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 go get github.com/sfproductlabs/roo
 #go install github.com/sfproductlabs/roo
-CGO_LDFLAGS="-lrocksdb" GODEBUG=netdns=cgo+1  go build -v -o rood github.com/sfproductlabs/roo/v3/roo
+CGO_LDFLAGS="-lrocksdb" GODEBUG=netdns=cgo go build -v -o rood github.com/sfproductlabs/roo/v3/roo
 sudo docker build -t roo .
