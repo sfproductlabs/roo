9d7dae08d8bd49988f2860787319f3e8c43bdb13
Andrew Grosser <dioptre@gmail.com>
2020-04-14 11:23:59 -0700

    wip

--- roo/kv.go
+++ roo/kv.go
@@ -298,7 +298,6 @@ rejoin:
 
 	go func() {
 		for {
-			time.Sleep(time.Duration(7) * time.Second)
 			action := &KVAction{
 				Action: PUT,
 				Key:    PEER_PREFIX + kvs.AppConfig.Cluster.Binding + NODE_POSTFIX,
@@ -308,6 +307,7 @@ rejoin:
 			defer cancel()
 			if _, err := kvs.execute(ctx, action); err != nil {
 				rlog.Infof("Adding node to kv didn't happen yet: %s probably still waiting for cluster\n", err)
+				time.Sleep(time.Duration(7) * time.Second)
 				continue
 			}
 			started := &KVAction{
@@ -317,6 +317,7 @@ rejoin:
 			}
 			if _, err := kvs.execute(ctx, started); err != nil {
 				rlog.Infof("Adding cluster started value failed\n", err)
+				time.Sleep(time.Duration(1) * time.Second)
 				continue
 			}
 			rlog.Infof("[[CREATED NODE]]\n")
