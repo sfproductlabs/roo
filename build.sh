a76425c1bbad38129856f3fc1b1c6ff0c8e0a68f
Andrew Grosser <dioptre@gmail.com>
2020-04-13 22:15:08 -0700

    wip

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 go get github.com/sfproductlabs/roo
 #go install github.com/sfproductlabs/roo
+str=`egrep "Roo. Version [0-9]+" ./roo/roo.go`
+oldver=$(echo $str | tr -d "Roo. Version ")
+newver=`expr $oldver + 1`
+sed -i "" -E "s/Roo. Version $oldver\$/Roo. Version $newver/g" "./roo/roo.go"
 CGO_LDFLAGS="-lrocksdb" GODEBUG=netdns=cgo go build -v -o rood github.com/sfproductlabs/roo/v3/roo
 sudo docker build -t roo .
