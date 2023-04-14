add5f722d8458d44432392a21b0945b2eea9206c
Andrew Grosser <dioptre@gmail.com>
2023-04-13 18:33:45 -0700

    building in mac

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# cat ~/.DH_TOKEN | docker login --username sfproductlabs --password-stdin
-# go get github.com/sfproductlabs/roo
-# #go install github.com/sfproductlabs/roo
-# oldver=`grep -oP '(?<=Roo\.\ Version\ ).*(?=\")' ./roo/roo.go`
-# newver=`expr $oldver + 1`
-# sed -i "s/Roo\.\ Version\ $oldver/Roo\.\ Version\ $newver/g" "./roo/roo.go"
-# sudo rm -rf cluster-data
-# #CGO_LDFLAGS="-lrocksdb" go build -v -o rood github.com/sfproductlabs/roo/v3/roo
-# go build -v -o rood github.com/sfproductlabs/roo/v4/roo
+oldver=`perl -nle'print for /Roo\.\ Version\ (.*)\".*$/g' ./roo/roo.go`
+newver=`expr $oldver + 1`
+sed -i -- "s/Roo\.\ Version\ $oldver/Roo\.\ Version\ $newver/g" "./roo/roo.go"
+bash -c "rm roo/roo.go-- || exit 0"
+sudo rm -rf cluster-data*
+sudo rm -rf roo/cluster-data*
 sudo docker build -t roo .
 #IF THE ABOVE FAILS
 #error getting credentials - err: exit status 1
